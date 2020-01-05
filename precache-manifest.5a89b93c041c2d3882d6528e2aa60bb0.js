6dfd193e5251053b993070efde86e34753365f81
Julien <jcvolan@gmail.com>
2020-01-05 21:27:32 +0100

    Updates

---
+++ precache-manifest.5a89b93c041c2d3882d6528e2aa60bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c8ef2d35f4714ece48a9f2e88abbc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b0034eb7b31c5b5c46b",
+    "url": "/static/js/2.870304f5.chunk.js"
+  },
+  {
+    "revision": "eb915343906e6100ab3935150ff8b71f",
+    "url": "/static/js/2.870304f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "668b082193a53b0a8d43",
+    "url": "/static/js/main.0252e330.chunk.js"
+  },
+  {
+    "revision": "47d5e18bfb3217f3f972",
+    "url": "/static/js/runtime-main.1b01ccc2.js"
+  }
+]);
